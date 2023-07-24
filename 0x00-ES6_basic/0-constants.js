7f40504c7b48dc802085c467c4cf5717354794b2
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-07-24 11:56:52 +0300

    first commit

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+const function taskFirst() {
+  var task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+let function taskNext() {
+  var combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
