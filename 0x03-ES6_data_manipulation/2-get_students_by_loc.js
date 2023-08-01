ed4cb416816ff8bc17ec2cee2cab33ebb53d7502
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-08-01 22:15:07 +0300

    eslint

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 export default function getStudentsByLocation(arr, city) {
-    if (arr instanceof Array) {
-        return arr.filter((cities) => cities.location === city);
-    }
-    return [];
+  if (arr instanceof Array) {
+    return arr.filter((cities) => cities.location === city);
+  }
+  return [];
 }
